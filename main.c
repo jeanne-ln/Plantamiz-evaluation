7bea0c701fa79b18ed9fec1b92c83cd32ad4a22c
jeanne <leonie.hinard@gmail.com>
2024-06-16 20:25:01 +0200

    lecture fichier

--- main.c
+++ main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "evaluation.h"
 
+char tab[LARGEUR][HAUTEUR];
+
+void lecturetest(){
+    FILE *f=fopen("test", "r");
+    if(!f){
+        printf("impossible d'ouvrir le fichier");
+        exit(1);
+    }
+    for(int ligne=0;ligne<HAUTEUR;ligne++){
+        for(int colonne=0;colonne<LARGEUR;colonne++){
+            fscanf(f,"%c",&tab[colonne][ligne]);
+        }
+    }
+    fclose(f);
+}
+
+void affichage(){
+    for(int ligne=0;ligne<HAUTEUR;ligne++){
+        for(int colonne=0;colonne<LARGEUR;colonne++){
+            printf("%c",tab[colonne][ligne]);
+        }
+        //printf("\n");
+    }
+}
+
+
 int main(void) {
-    printf("Hello\n");
+    lecturetest();
+    affichage();
+
     return 0;
 }
