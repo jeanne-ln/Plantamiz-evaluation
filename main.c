94c746e0010c237ecad2e4cc15f7739078954150
jeanne <leonie.hinard@gmail.com>
2024-06-19 10:24:03 +0200

    lecture avec getline

--- main.c
+++ main.c
@@ -5,14 +5,45 @@
 char tab[LARGEUR][HAUTEUR];
 
 void lecturetest(){
+    char *texte = NULL;
+    long longueur = 0;
     FILE *f=fopen("test", "r");
     if(!f){
         printf("impossible d'ouvrir le fichier");
         exit(1);
     }
     for(int ligne=0;ligne<HAUTEUR;ligne++){
+        if(getline(&texte, &longueur, f) == -1) {
+            printf("il manque la ligne numéro %d\n", ligne);
+            exit(1);
+        }
         for(int colonne=0;colonne<LARGEUR;colonne++){
-            fscanf(f,"%c",&tab[colonne][ligne]);
+            char symbole = texte[colonne];
+            switch(symbole) {
+                case 'S':
+                case 's':
+                    tab[colonne][ligne] = 'S';
+                    break;
+                case 'F':
+                case 'f':
+                    tab[colonne][ligne] = 'F';
+                    break;
+                case 'O':
+                case 'o':
+                    tab[colonne][ligne] = 'O';
+                    break;
+                case 'P':
+                case 'p':
+                    tab[colonne][ligne] = 'P';
+                    break;
+                case 'M':
+                case 'm':
+                    tab[colonne][ligne] = 'M';
+                    break;
+                default:
+                  printf("symbole inconnu ligne %d colonne %d : '%c'\n", ligne, colonne, symbole);
+                  exit(1);
+            }
         }
     }
     fclose(f);
@@ -23,7 +54,7 @@ void affichage(){
         for(int colonne=0;colonne<LARGEUR;colonne++){
             printf("%c",tab[colonne][ligne]);
         }
-        //printf("\n");
+        printf("\n");
     }
 }
 
