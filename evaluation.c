92972f7cbfc435c8fe4b3150d0cf4dfac86e51d4
jeanne <leonie.hinard@gmail.com>
2024-06-20 21:43:28 +0200

    ligne horizontale + vertical

--- evaluation.c
+++ evaluation.c
@@ -31,10 +31,42 @@ combinaison* evaluation(char tab[LARGEUR][HAUTEUR]){
                         }
                     }
                 }
-            }else if(longueur>=4) {
+            }else if(longueur>=3) {
                 score=longueur;
                 for (int j = colonne-longueur; j<colonne; j++){
                     resultat.elimination[j][ligne] = 1;
+                }
+            }
+            switch(symbole) {
+                case 'S': resultat.nb_soleils += score; break;
+                case 'F': resultat.nb_fraises += score; break;
+                case 'M': resultat.nb_mandarine += score; break;
+                case 'O': resultat.nb_oignons += score; break;
+                case 'P': resultat.nb_pommes += score; break;
+            }
+        }
+    }
+    for(int colonne=0;colonne<LARGEUR;colonne++){
+        for(int ligne=0;ligne<HAUTEUR;ligne++){
+            char symbole=tab[colonne][ligne];
+            int longueur=1;
+            int score=0;
+            while (ligne<HAUTEUR && tab[colonne][++ligne] == symbole) {
+                longueur++;
+            }
+            if (longueur >= 6) {
+                for(int i=0;i<LARGEUR;i++){
+                    for(int j=0;j<HAUTEUR;j++){
+                        if(tab[j][i]==symbole){
+                            score++;
+                            resultat.elimination[j][i]=1;
+                        }
+                    }
+                }
+            }else if(longueur>=3) {
+                score=longueur;
+                for (int j = ligne-longueur; j<ligne; j++){
+                    resultat.elimination[colonne][j] = 1;
                 }
             }
             switch(symbole) {
