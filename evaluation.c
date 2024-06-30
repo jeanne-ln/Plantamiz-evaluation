bdaf568c7e3c779643b0e7ab67fe05bea289d837
jeanne <leonie.hinard@gmail.com>
2024-06-30 21:23:20 +0200

    commentaires

--- evaluation.c
+++ evaluation.c
@@ -22,26 +22,26 @@ score* evaluation(plateau* ptab) {
             trous[colonne][ligne] = 0;
         }
     }
-    resultat.nb_soleils = 0;
+    resultat.nb_soleils = 0;//initialisation du score
     resultat.nb_fraises = 0;
     resultat.nb_mandarine = 0;
     resultat.nb_oignons = 0;
     resultat.nb_pommes = 0;
-    motif_horizontal(ptab, trous);
+    motif_horizontal(ptab, trous);//recherche des motifs
     motif_vertical(ptab, trous);
     motif_h(ptab, trous);
     motif_rect(ptab, trous);
     for (int ligne = 0; ligne < HAUTEUR; ligne++) {
         for (int colonne = 0; colonne < LARGEUR; colonne++) {
             if(trous[colonne][ligne]){
-                (*ptab)[colonne][ligne]=' ';
+                (*ptab)[colonne][ligne]=' ';// affichage des trous
             }
         }
     }
     if(resultat.nb_soleils +resultat.nb_fraises + resultat.nb_mandarine + resultat.nb_oignons + resultat.nb_pommes){
         return &resultat;
     }
-    return 0;
+    return 0;// si il n'y a pas de points, il n'y a pas de combinaisons donc pas de trous
 }
 
 void MAJ_score(char symbole, int score){
@@ -54,16 +54,16 @@ void MAJ_score(char symbole, int score){
     }
 }
 
-void motif_horizontal(plateau* ptab, int trous[LARGEUR][HAUTEUR]){
-    for(int ligne=0;ligne<HAUTEUR;ligne++){
+void motif_horizontal(plateau* ptab, int trous[LARGEUR][HAUTEUR]){// recherche de motifs horizontal
+    for(int ligne=0;ligne<HAUTEUR;ligne++){ //on parcours le tableau
         for(int colonne=0;colonne<LARGEUR;){
-            char symbole=(*ptab)[colonne][ligne];
-            int longueur=1;
+            char symbole=(*ptab)[colonne][ligne]; //on regarde le symbole de la case
+            int longueur=1; // initialisation de la longueur
             int score=0;
             while (colonne++<LARGEUR-1 && (*ptab)[colonne][ligne] == symbole) {
-                longueur++;
+                longueur++;//on regarde les symboles à coté, tant que ce sont les même on augmente la valeur
             }
-            if (longueur >= 6) {
+            if (longueur >= 6) {// si la longueur est plus grande que 6 on enlève tous les élements du même type du tableau
                 for(int i=0;i<HAUTEUR;i++){
                     for(int j=0;j<LARGEUR;j++){
                         if((*ptab)[j][i]==symbole){
@@ -112,7 +112,7 @@ void motif_vertical(plateau* ptab, int trous[LARGEUR][HAUTEUR]){
     }
 }
 
-int haut(plateau* ptab, int colonne,int ligne){
+int haut(plateau* ptab, int colonne,int ligne){// calcul d'une longueur vers le haut
     char symbole=(*ptab)[colonne][ligne];
     int longueur=1;
     while (ligne-->0 && (*ptab)[colonne][ligne] == symbole) {
@@ -121,7 +121,7 @@ int haut(plateau* ptab, int colonne,int ligne){
     return longueur;
 }
 
-int bas(plateau* ptab, int colonne,int ligne){
+int bas(plateau* ptab, int colonne,int ligne){//calcul d'une longueur vers le bas
     char symbole=(*ptab)[colonne][ligne];
     int longueur=1;
     while (ligne++<HAUTEUR-1 && (*ptab)[colonne][ligne] == symbole) {
@@ -130,7 +130,7 @@ int bas(plateau* ptab, int colonne,int ligne){
     return longueur;
 }
 
-int gauche(plateau* ptab, int colonne,int ligne){
+int gauche(plateau* ptab, int colonne,int ligne){//calcul d'une longueur vers la gauche
     char symbole=(*ptab)[colonne][ligne];
     int longueur=1;
     while (colonne-->0 && (*ptab)[colonne][ligne] == symbole) {
@@ -139,7 +139,7 @@ int gauche(plateau* ptab, int colonne,int ligne){
     return longueur;
 }
 
-int droite(plateau* ptab, int colonne,int ligne){
+int droite(plateau* ptab, int colonne,int ligne){//calcul d'une longueur vers la droite
     char symbole=(*ptab)[colonne][ligne];
     int longueur=1;
     while (colonne++<LARGEUR-1 && (*ptab)[colonne][ligne] == symbole) {
@@ -148,7 +148,7 @@ int droite(plateau* ptab, int colonne,int ligne){
     return longueur;
 }
 
-int min(int x, int y){
+int min(int x, int y){// calcul du minimum de deux nombres
     if(x>y) return y;
     else return x;
 }
@@ -188,25 +188,25 @@ void motif_h(plateau* ptab, int trous[LARGEUR][HAUTEUR]){
 
 
 void motif_rect(plateau* ptab, int trous[LARGEUR][HAUTEUR]){
-    for(int ligne=0;ligne<HAUTEUR;ligne++){
+    for(int ligne=0;ligne<HAUTEUR;ligne++){// on parcours le tableau
         for(int colonne=0;colonne<LARGEUR;colonne++){
-            int hauteur_max=bas(ptab, colonne,ligne);
-            int largeur_max=droite(ptab,colonne,ligne);
-            if(hauteur_max<2 || largeur_max<2){
+            int hauteur_max=bas(ptab, colonne,ligne);//on regarde la longueur vers le bas
+            int largeur_max=droite(ptab,colonne,ligne);// et la longueur vers la droite
+            if(hauteur_max<2 || largeur_max<2){// si la hauteur ou la largeur est plus petite que 2 il n'y a pas de rectangle on passe à la case suivante
                 continue;
             }
-            for(int l=ligne+1;l<=ligne+hauteur_max-1;l++){
+            for(int l=ligne+1;l<=ligne+hauteur_max-1;l++){//on parcours le rectangle formé par la hauteur et la largeur trouvé
                 for(int c=colonne+1;c<=colonne+largeur_max-1;c++){
-                    int hauteur=l-ligne+1;
+                    int hauteur=l-ligne+1;//on calcul la hauteur et la largeur necessaire pour former un rectangle
                     int largeur=c-colonne+1;
-                    if(haut(ptab,c,l)<hauteur){
+                    if(haut(ptab,c,l)<hauteur){//si la hauteur actuelle est plus petite que la hauteur necessaire il n'y a pas de rectangle
                         continue;
                     }
-                    if(gauche(ptab,c,l)<largeur){
+                    if(gauche(ptab,c,l)<largeur){//si la largeur actuelle est plus petite que la largeur necessaire il n'y a pas de rectangle
                         continue;
                     }
                     //on a un rectangle !!
-                    for(int lig=ligne;lig<=ligne+hauteur-1;lig++){
+                    for(int lig=ligne;lig<=ligne+hauteur-1;lig++){//mise à jour du tableau des trous
                         trous[colonne][lig]=1;
                         trous[colonne+largeur-1][lig]=1;
                     }
@@ -214,7 +214,7 @@ void motif_rect(plateau* ptab, int trous[LARGEUR][HAUTEUR]){
                         trous[col][ligne]=1;
                         trous[col][ligne+hauteur-1]=1;
                     }
-                    int score=2*hauteur*largeur;
+                    int score=2*hauteur*largeur;// calcul du score
                     char symbole=(*ptab)[colonne][ligne];
                     MAJ_score(symbole,score);
                 }
--- jeux.c
+++ jeux.c
@@ -16,9 +16,9 @@ int affiche_temps(time_t start_time, int temps_limite);
 
 //contrats non-utilisé dans le code
 const Contrat tab_contrats[NOMBRE_CONTRATS] = {
-        {{10,10,10,10,10}, 75, 300},
-        {{30,30,30,30,30}, 50, 250},
-        {{40,40,40,40,40}, 40, 200}
+        {{10,10,10,10,10}, 20, 300},
+        {{30,30,30,30,30}, 20, 250},
+        {{40,40,40,40,40}, 20, 200}
 };
 
 
@@ -41,62 +41,62 @@ Partie* nouvelle_partie(){//création d'une nouvelle partie
 
     return partie;
 }
-void jouer(Partie* partie){
+void jouer(Partie* partie){//fonction pour jouer
     position curseur;
     int selection=0;
-    curseur.colonne=0;
+    curseur.colonne=0;//initialisation de la position du curseur
     curseur.ligne=0;
-    affiche(&partie->tab,selection, &curseur,&partie->points,partie->nb_coup_restant);
-    time_t start_time;
-    start_time = time(NULL);
+    affiche(&partie->tab,selection, &curseur,&partie->points,partie->nb_coup_restant);//affichage du plateau
+    time_t start_time;//initialisation du temps
+    start_time = time(NULL);//démarrage du chronomètre
     int arret=0;
-    while(!arret || partie->nb_coup_restant<=0){
-        arret=affiche_temps(start_time, 120);
+    while(!arret || partie->nb_coup_restant<0){// si le minuteur est déppasé ou si le nombre de coup est dépassé on arrête
+        arret=affiche_temps(start_time, 120);//affichage du temps : 120 secondes
         if (keypressed()) {
             position precedent;
             precedent.colonne=curseur.colonne;
             precedent.ligne=curseur.ligne;
-            int touche = readkey() >> 8;
-            if (touche == KEY_UP) {
-                if (curseur.ligne == 0) {
+            int touche = readkey() >> 8; //lecture de la touche
+            if (touche == KEY_UP) {// déplacement vers le haut
+                if (curseur.ligne == 0) {// sauf si on est tout en haut, on revient en bas
                     curseur.ligne = HAUTEUR - 1;
                     selection=0;
                 }else{
                     curseur.ligne--;
                 }
             }
-            if (touche == KEY_DOWN) {
-                if (curseur.ligne == HAUTEUR-1) {
+            if (touche == KEY_DOWN) {// déplacement vers le bas
+                if (curseur.ligne == HAUTEUR-1) {//sauf si on est tout en bas, on revient en haut
                     curseur.ligne = 0;
                     selection=0;
                 }else{
                     curseur.ligne++;
                 }
             }
-            if (touche == KEY_RIGHT) {
-                if (curseur.colonne == LARGEUR-1) {
+            if (touche == KEY_RIGHT) {// déplacement vers la droite
+                if (curseur.colonne == LARGEUR-1) {//sauf si on est tout à droite, on revient à gauche
                     curseur.colonne = 0;
                     selection=0;
                 }else{
                     curseur.colonne++;
                 }
             }
-            if (touche == KEY_LEFT) {
-                if (curseur.colonne == 0) {
+            if (touche == KEY_LEFT) {//déplacement vers la gauche
+                if (curseur.colonne == 0) {//sauf si on est tout à gauche, on revient à droite
                     curseur.colonne = LARGEUR-1;
                     selection=0;
                 }else{
                     curseur.colonne--;
                 }
             }
-            if(touche == KEY_ESC){
+            if(touche == KEY_ESC){//si on appui sur échappe on sauvegarde la partie et on revient au menu
                 sauvegarder_jeu("sauvegarde_plantamiz.txt",partie);
                 arret=1;
             }
-            if(touche== KEY_SPACE){
+            if(touche== KEY_SPACE){// selection
                 selection=1-selection;
             }
-            if(selection && (touche==KEY_DOWN || touche==KEY_UP || touche==KEY_LEFT || touche==KEY_RIGHT)){
+            if(selection && (touche==KEY_DOWN || touche==KEY_UP || touche==KEY_LEFT || touche==KEY_RIGHT)){//permutation si on selectionne un objet et qu'on appui sur une flèche
                 selection=0;
                 echange(&partie->tab, &precedent, &curseur);
                 score* delta_score=evaluation(&partie->tab);
@@ -104,14 +104,14 @@ void jouer(Partie* partie){
                 if(delta_score){
                     mise_a_jour_score(delta_score,partie);
                     affiche(&partie->tab,selection, &curseur,&partie->points,partie->nb_coup_restant);
-                    affiche_temps(start_time, 60);
+                    affiche_temps(start_time, 120);
 
                     while(1) {
                         sleep(1);
                         bouche_les_trous(&partie->tab);
                         delta_score = evaluation(&partie->tab);
                         affiche(&partie->tab,selection, &curseur,&partie->points,partie->nb_coup_restant);
-                        affiche_temps(start_time, 60);
+                        affiche_temps(start_time, 120);
                         if(!delta_score) break;
                         mise_a_jour_score(delta_score,partie);
                     }
@@ -143,8 +143,8 @@ int affiche_temps(time_t start_time, int temps_limite){
     int ecoule;
     ecoule = (int)(current_time - start_time);
     temps_restant = temps_limite - ecoule;
-    if(precedent!=temps_restant) {
-        rectfill(screen, 120, 570, 800, 600, makecol(0, 0, 0));
+    if(precedent!=temps_restant) {//on réaffiche le temps seulement si la nombre à changer
+        rectfill(screen, 120, 570, 800, 600, makecol(0, 0, 0));//rectangle noir pour recouvrir le nombre précédent
         textprintf_ex(screen, font, 10, 575, makecol(255, 255, 255), -1, "temps restant: %d", temps_restant);
         precedent=temps_restant;
     }
